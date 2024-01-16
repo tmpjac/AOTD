276f4db922742e2b5a2441959fdb8b629dc0a9e0
jake <tmpjac@gmail.com>
2024-01-15 22:17:35 -0500

    port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 var bodyParser = require('body-parser')
 const session = require('express-session');
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
